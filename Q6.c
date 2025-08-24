b529333674aa2faca221c52a7b98f1a418cfd9ad
harshil-1207 <dharshil1297@gmail.com>
2025-08-24 16:15:25 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,10 @@
+//6. Convert Hours Into Minutes
+#include<stdio.h>
+int main(){
+float hours, minutes;
+    printf("Enter Time In Hours:");  // enter input
+    scanf("%f",&hours);
+ minutes = hours*60;  // conversion formula
+    printf("Minutes equal to: %f",minutes);
+    return 0;
+    }

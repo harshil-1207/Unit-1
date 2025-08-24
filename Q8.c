7b269a0dd86913ad7ff2b2eca0ecf60702e61ea7
harshil-1207 <dharshil1297@gmail.com>
2025-08-24 17:17:38 +0530

    Add files via upload

---
+++ Q8.c
@@ -0,0 +1,10 @@
+//8. Convert Dollars Into Rupees
+#include<stdio.h>
+int main(){
+float rupees , dollars;
+    printf("US Dollars :");  //enter input
+    scanf("%f",&dollars);
+rupees = 48*dollars; //conversion formula
+    printf("Rupees Equals To : %f",rupees);
+    return 0;
+}

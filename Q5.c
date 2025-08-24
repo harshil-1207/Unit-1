22baca6bf8b0eb70d45a10f37191353de94a4b90
harshil-1207 <dharshil1297@gmail.com>
2025-08-24 15:08:12 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,11 @@
+//5. Add, Subtract, Multiply, Divide Two Numbers
+
+#include<stdio.h>
+int main(){
+int a = 4, b = 2;
+    printf("addition : %d",a+b); //addition
+    printf("\nsubtraction : %d",a-b); //subtraction
+    printf("\nmultiplication : %d\n",a*b); //multiply
+    printf("division : %d",a/b); //divide
+    return 0;
+}
---
+++ Q6.c
@@ -0,0 +1,10 @@
+//6. Convert Hours Into Minutes
+#include<stdio.h>
+int main(){
+int hours, minutes;
+    printf("Enter Time In Hours:");  // enter input
+    scanf("%d",&hours);
+ minutes = hours*60;  // conversion formula
+    printf("Minutes equal to: %d",minutes);
+    return 0;
+    }
